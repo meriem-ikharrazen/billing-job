cb86160a1226594e2718244b10515dd13c0f8321
Meriem Ikharrazen <meriem.ikharrazen@gmail.com>
2023-10-24 15:49:35 +0200

    handling erros in jobs

--- src/main/java/com/example/billingjob/BillingJob.java
+++ src/main/java/com/example/billingjob/BillingJob.java
@@ -1,9 +1,19 @@
 package com.example.billingjob;
 
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.repository.JobRepository;
 
 public class BillingJob implements Job {
+
+    private JobRepository jobRepository;
+
+    public BillingJob(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
     @Override
     public String getName() {
         return "BillingJob";
@@ -12,6 +22,23 @@ public class BillingJob implements Job {
     @Override
     public void execute(JobExecution execution) {
         // TODO implement business logic
+        /*
+        Création d'un nv job
         System.out.println("Processing billing information");
+        execution.setStatus(BatchStatus.COMPLETED);
+        execution.setExitStatus(ExitStatus.COMPLETED);
+        this.jobRepository.update(execution);
+        */
+
+        //Handling error
+        try {
+            throw new Exception("Unable to process billing information");
+        } catch (Exception exception) {
+            execution.addFailureException(exception);
+            execution.setStatus(BatchStatus.COMPLETED);
+            execution.setExitStatus(ExitStatus.FAILED.addExitDescription(exception.getMessage()));
+        } finally {
+            this.jobRepository.update(execution);
+        }
     }
 }
